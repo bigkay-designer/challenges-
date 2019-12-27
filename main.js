5644d24b6dd6016744e2faa22bce91fa5c090089
khalid <ibrahimkhalid478@gmail.com>
2019-12-27 04:53:20 +0000

    initial

--- main.js
+++ main.js
@@ -17,15 +17,31 @@ reset.addEventListener('click', resting)
 function data(){
 
     let content = prompt(' what is your age? ');
-    content = content * allyear;
-    para.textContent = ' you are ' + content + ' days old';    para.style.backgroundColor = 'green';
+    // content = content * allyear;
+    // para.textContent = ' you are ' + content + ' days old';    
+    para.style.backgroundColor = 'green';
     para.style.color = 'white';
     para.style.width = '90%';
 
-    if (content < 1){
+    if (content >= 1){
+        content = content * allyear;
+        para.textContent = ' you are ' + content + ' days old';
+
+    }
+    else if (content < 1){
         status = 'invalid number, try again'
         para.textContent = status;
+
     }
+
+    else{
+        para.textContent = 'oops!! Put a number'
+    }
+
+
+    // else if(content === string){
+    //     string-error = ''
+    // }
 
 }
 
@@ -83,12 +99,11 @@ function list (){
 
 
 let bjGame = {
-    'you':{'scoreSpan':'#bj-yours-id', 'div':'#bj-yours', 'score':0},
-    'dealer':{'scoreSpan':'#bj-dealer-id', 'div':'#bj-dealer', 'score':0},
+    'you':{'scoreSpan': '#bj-your-id', 'div': '#bj-yours', 'score': 0},
+    'dealer':{'scoreSpan':'#bj-dealer-id', 'div':'#bj-dealer', 'score': 0},
     'cards' : ['2','3','4','5','6','7','8','9','10','K', 'Q','J','A'],
-    'cardMaps' : {'2': 2,'3':3,'4': 4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'K':10, 'Q':10,'J':10,'A':[1, 11]}
-}
-
+    'cardsMap' : {'2': 2, '3':3, '4': 4, '5':5, '6': 6, '7':7, '8':8, '9':9, '10':10, 'K':10, 'Q':10, 'J':10, 'A':[1, 11]}
+} 
 
 
 YOU =bjGame['you'];
@@ -96,19 +111,24 @@ DEALER =bjGame['dealer'];
 
 const btHitSound = new Audio('sounds/swish.m4a')
 
-document.querySelector('#bj-hit-btn').addEventListener('click', bjHitBtn)
+            // Event listenrs
 
+document.querySelector('#bj-hit-btn').addEventListener('click', bjHitBtn)
 document.querySelector('#bj-dealer-btn').addEventListener('click', bjDealBtn )
+document.querySelector('#bj-stand-btn').addEventListener('click', computer )
+document.querySelector('#bj-stand-btn').addEventListener('click', bjStandBtn)
 
             // Hit btn main function
 
 function bjHitBtn(){
     let card = randomCard();
-    console.log(card)
+    //console.log(card)
     showCard(card, YOU)
 
     updateScore(card, YOU)
-    console.log(updateScore['score'])
+    //console.log(YOU['score'])
+    showScore(YOU)
+    
 }
 
         //Generating random number
@@ -119,7 +139,8 @@ function randomCard(){
 }
         // show card function
 
-function showCard(card, activePlayer){
+function showCard(card, activePlayer){  
+    if (activePlayer['score'] <=21){
     const cardImage = document.createElement('img');
     cardImage.src = `bj-images/${card}.png`;
     cardImage.style.width = '80px'
@@ -127,6 +148,7 @@ function showCard(card, activePlayer){
     cardImage.style.paddingRight = '10px'
     document.querySelector(activePlayer['div']).appendChild(cardImage);
     btHitSound.play()
+    }
 }
 
         //Deal btn function
@@ -141,8 +163,73 @@ function bjDealBtn(){
     for(i=0; i < delIMages2.length; i++){
         delIMages2[i].remove()
     }
-}
-function updateScore (card, activePlayer){
-    activePlayer['score']+= bjGame['cardMaps'][card];
+
+    YOU['score'] = 0;
+    DEALER['score'] = 0
+    
+    document.querySelector('#bj-your-id').textContent = 0;
+    document.querySelector('#bj-dealer-id').textContent = 0;
+
+    
 }
 
+            // update score
+
+function updateScore (card, activePlayer){
+
+    if(card === 'A'){
+
+        if(activePlayer['score'] + bjGame['cardsMap'][card][1] <= 21){
+            activePlayer['score'] += bjGame['cardsMap'][card][1]
+
+        } else{
+            activePlayer['score'] += bjGame['cardsMap'][card][0];
+        }
+        
+
+    }
+    else{
+        activePlayer['score'] += bjGame ['cardsMap'] [card]
+    }
+
+   
+
+}
+
+
+
+function showScore(activePlayer ){
+    if(activePlayer['score'] > 21){
+        document.querySelector(activePlayer['scoreSpan']).textContent = 'busted'
+
+    }
+
+    else if(activePlayer['score'] < 21){
+
+        document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score']
+    }
+    
+    
+}
+        //stand button
+
+
+function bjStandBtn(){
+
+    let stand = document.querySelector('#bj-hit-btn');
+    //stand.removeEventListener('click', bjHitBtn)
+    // stand.remove(document.querySelector('#bj-hit-btn'))
+
+
+
+}
+
+function computer(){
+    let card = randomCard();
+    //console.log(card)
+    showCard(card, DEALER)
+
+    updateScore(card, DEALER)
+    console.log(DEALER['score'])
+    showScore(DEALER)
+}
